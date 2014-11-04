8a039474aee64606ef8419652027dfad4e0f331b
Evan Simoni <evan.simoni@gmail.com>
2014-11-04 01:24:11 -0800

    adds firebase simple login script and the auth controller

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -33,4 +33,16 @@ angular.module('myApp.controllers', [])
     party.notified = 'Yes';
     $scope.parties.$save(party.$id);
   };
+}])
+.controller('AuthController', ['$scope', '$firebaseSimpleLogin', function($scope, $firebaseSimpleLogin) {
+  var authRef = new Firebase('https://focalhost.firebaseio.com/');
+  var auth = $firebaseSimpleLogin(authRef)
+
+  $scope.user = {email: '', password: ''};
+
+  $scope.register = function() {
+    auth.$createUser($scope.user.email, $scope.user.password).then(function(data) {
+      console.log(data);
+    });
+  };
 }]);
